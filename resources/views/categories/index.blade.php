330e8cc66cf3f72300db7c6e7bc1286cbd133c52
Fredo <fredericbroncard@gmail.com>
2021-01-04 02:24:12 +0100

    Création des partials
    créations des partials du template et mise en place d'une première route

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,39 @@
+
+<ul class="list cat-list">
+    <li>
+        <a href="#" class="d-flex">
+            <p>Restaurant food</p>
+            <p>(37)</p>
+        </a>
+    </li>
+    <li>
+        <a href="#" class="d-flex">
+            <p>Travel news</p>
+            <p>(10)</p>
+        </a>
+    </li>
+    <li>
+        <a href="#" class="d-flex">
+            <p>Modern technology</p>
+            <p>(03)</p>
+        </a>
+    </li>
+    <li>
+        <a href="#" class="d-flex">
+            <p>Product</p>
+            <p>(11)</p>
+        </a>
+    </li>
+    <li>
+        <a href="#" class="d-flex">
+            <p>Inspiration</p>
+            <p>21</p>
+        </a>
+    </li>
+    <li>
+        <a href="#" class="d-flex">
+            <p>Health Care (21)</p>
+            <p>09</p>
+        </a>
+    </li>
+</ul>
---
+++ resources/views/template/partials/_main.blade.php
@@ -0,0 +1,36 @@
+<main>
+    <!--================Blog Area =================-->
+    <section class="blog_area section-padding">
+        <div class="container">
+            <div class="row">
+                @yield('content')
+                <div class="col-lg-4">
+                    <div class="blog_right_sidebar">
+                        <aside class="single_sidebar_widget search_widget">
+                            <form action="#">
+                                <div class="form-group">
+                                    <div class="input-group mb-3">
+                                        <input type="text" class="form-control" placeholder='Search Keyword'
+                                               onfocus="this.placeholder = ''"
+                                               onblur="this.placeholder = 'Search Keyword'">
+                                        <div class="input-group-append">
+                                            <button class="btns" type="button"><i class="ti-search"></i></button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button class="button rounded-0 primary-bg text-white w-100 btn_1 boxed-btn"
+                                        type="submit">Search</button>
+                            </form>
+                        </aside>
+
+                        @include('template.partials._single_sidebar_widget post_category_widget')
+                        @include('template.partials._single_sidebar_widget tag_cloud_widget')
+
+                        @include('template.partials._single_sidebar_widget newsletter_widget')
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================Blog Area =================-->
+</main>
